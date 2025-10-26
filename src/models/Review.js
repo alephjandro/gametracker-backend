407005f9962bf57948070edee0df108d664d6a67
Alejandro <alejobomu@gmail.com>
2025-10-26 14:40:13 -0500

    Actualización del archivo corrregido

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const gamesSchema = new MongoTopologyClosedError.Schema({
-    titulo: { type: String, required: true },
-    genero: { type: String, required: true },
-    plataforma: { type: String, required: true },
-    añolanzamiento: { type: Number, required: true },
-    desarrollador: { type: String, required: true },
-    imagenPortada: { type: String},
-    descripcion: { type: String},
-    completado: { type: Boolean, default: false },
-    fechaCreacion: { type: Date, default: Date.now }
+
+const reviewSchema = new mongoose.Schema({
+    juegoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true },
+    puntiacion: { type: Number, required: true, min: 1, max: 5 },
+    textoReseña: { type: String },
+    horasJugadas: { type: Number, default: 0 },
+    dificultad: { type: String, enum: ['Fácil', 'Normal', 'Difícil'] },
+    recomendariado: { type: Boolean},
+    fechaCreacion: { type: Date, default: Date.now },
+    fechaActualizacion: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Game', gamesSchema);
+module.exports = mongoose.model('Review', gamesSchema);
