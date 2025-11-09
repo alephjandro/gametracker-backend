a8594d6a9424bb1f67cc216a68003fec5a9ee26d
aleph_jandro <alephbolanosmunoz@gmail.com>
2025-11-09 16:13:05 -0500

    Actualizo archivos para incorporarlos en otra pc

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
+
 const reviewSchema = new mongoose.Schema({
-    juegoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true },
-    puntuacion: { type: Number, required: false, min: 1, max: 5 },
-    textoResena: { type: String },
-    horasJugadas: { type: Number, default: 0 },
-    dificultad: { type: String, enum: ['Fácil', 'Normal', 'Difícil'] },
-    recomendacion: { type: Boolean},
-    fechaCreacion: { type: Date, default: Date.now },
-    fechaActualizacion: { type: Date, default: Date.now }
+  juegoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true },
+  puntuacion: { type: Number, required: true, min: 1, max: 5 },
+  textoResena: { type: String, required: true },
+  horasJugadas: { type: Number, default: 0 },
+  dificultad: { type: String, enum: ['Fácil', 'Normal', 'Difícil'], default: 'Normal' },
+  recomendaria: { type: Boolean, default: false }, 
+  fechaCreacion: { type: Date, default: Date.now },
+  fechaActualizacion: { type: Date, default: Date.now }
+});
+
+reviewSchema.pre('findOneAndUpdate', function (next) {
+  this.set({ fechaActualizacion: new Date() });
+  next();
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
